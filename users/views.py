b45b9fb5f16d685271498fb0f236aea90991c69d
Islammarcelo <eslamadel2000@yahoo.com>
2020-04-01 23:06:54 +0200

    1-Developed a web service to let the user able to register in the online market and 2-Developed a web service to be used to list all current registered users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
+from rest_framework import generics
+
+User = get_user_model()
+
+class RegisterApiView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class ListUsersApiView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
