6cd49466016af9d608026302c0c5c9f62d919b6a
Islammarcelo <eslamadel2000@yahoo.com>
2020-04-16 08:21:35 +0200

    Added Function to enable admin only view list of uers

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 from django.contrib.auth import get_user_model
@@ -11,6 +12,11 @@ User = get_user_model()
 class RegisterApiView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
+class permissionsToViewList(IsAdminUser):
+    def has_permission(self,request,view):
+        return request.user.user_type == 'admin'
+
 class ListUsersApiView(generics.ListAPIView):
+    permission_classes = (permissionsToViewList,) 
     queryset = User.objects.all()
     serializer_class = UserSerializer
